bc69b140fc77c172af18c3f20c5bfb1c4e651461
David Conners <david@conners.com>
2021-01-25 21:46:44 -0800

    Added install from git hub option

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,4 @@
+param([switch]$Install)
 function prompt
 {
 
@@ -18,84 +19,45 @@ function prompt
 
 }
 
-function Ask-YesNo(
-                    [Parameter(Mandatory=$true)] 
-                    [String]
-                    $Question,
-                    [Parameter(Mandatory=$false)] 
-                    [String]
-                    $Title,
-                    [Parameter(Mandatory=$false)] 
-                    [ValidateSet("Yes","No")] 
-                    [String] 
-                    $Default = "No")
+function Update-Self
 {
-
-    $Options = ((New-Object System.Management.Automation.Host.ChoiceDescription "&Yes"),`
-                (New-Object System.Management.Automation.Host.ChoiceDescription "&No"))
-    
-    if ($Default -eq "Yes") {$DefaultOption = 0}
-    else {$DefaultOption = 1}
-    
-
-    $result = $host.ui.PromptForChoice($Title, $Question, $Options, $DefaultOption) 
-
-    switch ($result)
+    if (Test-Administrator)
     {
-        0 {return $true}
-        1 {return $false}
+        Write-Warning "In elevated prompt, not updating"
+        return
     }
-    
-}
 
-function install-self
-{
-    if (test-path $profile) {return}
-    if (Ask-YesNo -Question "No profile found, create profile referencing this script?" -Default Yes)
+    try
     {
-        Write-host -f Yellow "Installing self into profile"
-        write-host -f Yellow 'execute . $profile to load into memory or open new session'
-        $basicprofile = ". " + $MyInvocation.ScriptName
-        $basicprofile | Set-Content -Path $profile
+        $client = New-Object System.Net.WebClient
+        $client.DownloadFile("https://raw.githubusercontent.com/dconners/PowerShellProfile/master/profile.ps1",$profile)
+        Write-Host "Updated profile, please restart PowerShell"
     }
+    catch
+    {
+        Write-Error "Error updating profile`n$_"
+    }
+
 }
-
-# function go-to($shortcut)
-# {
-#     #Assuming that I have moved my 1DRVs to the same location as the Documents directory
-#     $Target = (get-itemproperty ([Environment]::GetFolderPath("MyDocuments"))).Parent.FullName
-
-
-#     $target = $null
-#     foreach ($match in gci -Path $Target -Recurse -Directory -Include $shortcut)
-#     {
-#         if (-not ($match.FullName.StartsWith("$home\Favorites")))
-#         {
-#             if ($target) {Write-Verbose "Ambiguous Destination";return}
-#             else {$target = $match}
-#         }
-#     }
-#     if ($target)
-#     {
-#         cd $target
-#     }
-#     else
-#     {
-#         write-verbose "Target not found"
-#     }
-# }
 
 function Test-Administrator
 {    
     return ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
 }
 
-
-# #Aliases
-# New-Alias goto Go-To
-
-if (!(Test-Administrator))
-{   
-    #don't want to install when elevated
-    install-self
+if ($Install)
+{
+    if (Test-Administrator)
+    {
+        Write-Warning "In elevated prompt, not installing"
+        return
+    }
+    try
+    {
+        Copy-Item -Path $MyInvocation.MyCommand.Path -Destination $profile
+    }
+    catch
+    {
+        Write-Error "Error installing profile`n$_"
+    }
 }
