92ff64d6672effddbaa0fd4c91ca78c8293df7d7
David Conners <david@conners.com>
2021-09-27 13:31:14 -0700

    change goto to use specified locations, added go back

--- profile.ps1
+++ profile.ps1
@@ -53,43 +53,63 @@ function goto
     param (
         [Parameter(Mandatory)]
         [string]
-        $Folder,
-        [int]
-        $Depth=4
+        $Folder
     )
-
-    $choices = Get-ChildItem -Path \ -Directory -Depth $Depth -Filter "$($folder)*" -ErrorAction SilentlyContinue | Sort-Object FullName
-
+    
+    #Get the list of directories to search
+    $searchFolders = @($env:USERPROFILE)
+    #get the script path
+    $scriptDir = Split-Path $MyInvocation.MyCommand.Path -Parent
+    if (Test-Path "$scriptDir\goto.lst")
+    {
+        Get-Content "$scriptDir\goto.lst" | ForEach-Object {$searchFolders += $_}
+    }
+    
+    $choices = @()
+    
+    foreach($searchFolder in $searchFolders)
+    {
+        Get-ChildItem -Path $searchFolder -Directory -Recurse -Filter "$($Folder)*" -ErrorAction SilentlyContinue | ForEach-Object {$choices += $_}
+    }
+    $choices = $choices | Sort-Object FullName
+    
     if ($choices.Count -eq 0)
     {
-        Write-Warning "No match found for $Folder at depth $Depth"
+        Write-Warning "No match found for $Folder"
         return
     }
-
+    
     if ($choices.Count -eq 1)
     {
+        #save the current location
+        Push-Location -StackName GoTo
         Set-Location -Path $choices[0]
         return
     }
-
+    
     if ($choices.Count -gt 9)
     {
         Write-Warning "Too many options"
         return
     }
-
-
+    
+    
     #We have 2-9 options
     for($i=0;$i -lt $choices.Count;$i++)
     {
-        Write-Host "$($i+1) $($choices[$i])"
+        Write-Host -ForegroundColor Yellow -NoNewline "$($i+1) "
+        Write-Host -ForegroundColor Green "$($choices[$i])"
     }
-
+    
     $choice = -1
     while($choice -lt 0)
     {
-        $userInput = Read-Host -Prompt "Select Folder (0 to exit)"
-        if ($userInput -notmatch "\d")
+        $userInput = Read-Host -Prompt "Select Folder (return to exit)"
+        if ($userInput -eq "")
+        {
+            $choice = 0
+        }
+        elseif ($userInput -notmatch "\d")
         {
             Write-Warning "Please select a number"
         }
@@ -104,12 +124,17 @@ function goto
     }
     if ($choice -gt 0)
     {
+        #save the current location
+        Push-Location -StackName GoTo
         Set-Location $choices[$choice-1]
-    }
+     }    
 
 }
 
-
+function goback
+{
+    Pop-Location -StackName GoTo
+}
 
 
 #Self install if we run the script with the appropriate switch
